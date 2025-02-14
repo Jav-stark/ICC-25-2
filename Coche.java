f614ef32ac38e6c80fc7fb59cef1ae6643fd2485
Jav-stark <javiermendoza@ciencias.unam.mx>
2025-02-13 23:29:41 -0600

    subo mi parte

---
+++ Coche.java
@@ -0,0 +1,26 @@
+public class Coche {
+     public static void main(String[] args) {
+        imprimirBienvenida();
+        Coche nissan2025 = new Coche (marca: "NIssan", modelo: "Sentra" , anio:2025);
+        // Simulamos el comportamiento de manejar un carro
+        nissan2025 , acelerar (incremento:50)
+        nissan2025 , frenar (decremento:30)
+
+        nissan2025 modelo = "Sentra2026";
+
+
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
